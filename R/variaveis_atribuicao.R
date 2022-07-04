5a97a2d4be4beb2cb2277088b5510225498d3c53
Fábio Júnior <fjumisan@gmail.com>
2022-07-04 10:21:55 -0300

    variavel e atribuicao

---
+++ R/variaveis_atribuicao.R
@@ -0,0 +1,20 @@
+
+
+a <- 10 #atribuição
+
+a #print
+
+b = 10 #outra forma de atribuição
+b
+
+10 -> c #atribuição sempre apontando para a variável
+
+d <- a + b
+d
+
+
+a <- "Fábio"
+b <- "Júnior"
+
+
+
